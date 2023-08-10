66cb81f0978749e4cf93a06b6b3b560fab6158cd
hamirf <haidar300400@gmail.com>
2023-08-10 20:26:26 +0700

    Add XML Docs to IBoard, ICheckers, and IPiece methods

--- CheckersGameLib/interface/IBoard.cs
+++ CheckersGameLib/interface/IBoard.cs
@@ -2,6 +2,16 @@ namespace CheckersGameLib;
 
 public interface IBoard
 {
+    /// <summary>
+    /// Retrieve board of game's size
+    /// </summary>
+    /// <returns>integer of board size</returns>
     int GetSize();
+
+    /// <summary>
+    /// Set the size of board of game
+    /// </summary>
+    /// <param name="size"></param>
+    /// <returns>true if set is success, otherwise false</returns>
     bool SetSize(int size);
 }
--- CheckersGameLib/interface/IPiece.cs
+++ CheckersGameLib/interface/IPiece.cs
@@ -2,9 +2,29 @@ namespace CheckersGameLib;
 
 public interface IPiece
 {
+    /// <summary>
+    /// Retrieve position of a checkers piece
+    /// </summary>
+    /// <returns>position of a checkers piece that contains its row and column</returns>
     Position GetPosition();
+
+    /// <summary>
+    /// Set the position of a checkers piece
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>true if set is success, otherwise false</returns>
     bool SetPosition(Position position);
 
+    /// <summary>
+    /// Retrieve color of a checkers piece
+    /// </summary>
+    /// <returns>whether black or red color of a piece</returns>
     PieceColor GetPieceColor();
+
+    /// <summary>
+    /// Set the color of a checkers piece
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns>true if set is success, otherwise false</returns>
     bool SetPieceColor(PieceColor color);
 }
